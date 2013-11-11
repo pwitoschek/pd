d097e0a08abf8d4eb31f1d21d6d2e5d1694ee3b8
pwitoschek <peter.witoschek@gmail.com>
2013-11-11 20:40:34 +0100

    model

---
+++ pd/src/main/java/de/app/model/Tagesertrag.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.app.model;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * @author Peter
+ *
+ */
+
+public class Tagesertrag {
+	private String weekDay;
+	private Date date;
+	private Time time;
+}
