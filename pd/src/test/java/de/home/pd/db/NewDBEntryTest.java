6a6605d53429e666a73c23480545f6af3d0d31cc
pwitoschek <p.witoschek@gmail.com>
2013-11-16 11:16:26 +0100

    jpa

---
+++ pd/src/test/java/de/home/pd/db/NewDBEntryTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package de.home.pd.db;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.app.pd.entity.pv.Tagesertrag;
+
+/**
+ * @author pd
+ * 
+ */
+public class NewDBEntryTest {
+
+	private static Logger logger = Logger.getLogger(NewDBEntryTest.class);
+
+	@Test
+	public void test() {
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("PDPersitenceUnit");
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+			Tagesertrag tagesertrag = new Tagesertrag();
+			tagesertrag.setDatum(new Date());
+			tagesertrag.setDurchschnittsTemperatur(4.0);
+			tagesertrag.setErzeugerStrom(10.0);
+			tagesertrag.setWochentag("Samstag");
+			em.persist(tagesertrag);
+			em.getTransaction().commit();
+			Assert.assertTrue(true);
+
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			logger.error(e.toString());
+			Assert.fail(e.toString());
+		}
+	}
+
+}
