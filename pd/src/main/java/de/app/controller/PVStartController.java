49d282a8bc8e388e0c47aa82e887abbbca97d6f0
pwitoschek <peter.witoschek@gmail.com>
2013-11-19 22:20:01 +0100

    nochmal

--- pd/src/main/java/de/app/controller/PVStartController.java
+++ pd/src/main/java/de/app/controller/PVStartController.java
@@ -5,13 +5,13 @@ package de.app.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import db.DataAccessObjectImpl;
 import de.app.interfaces.DataAccessObject;
 import de.app.pd.entities.pv.Tagesverbrauch;
 
@@ -23,11 +23,9 @@ import de.app.pd.entities.pv.Tagesverbrauch;
 @Controller
 public class PVStartController {
 
-	@Autowired
-	DataAccessObject dao;
-
 	@RequestMapping(value = "/pv/{function}", method = { RequestMethod.GET })
 	public ModelAndView getStartpage(@PathVariable String function) {
+		DataAccessObject dao = new DataAccessObjectImpl();
 		List<Tagesverbrauch> tagesverbrauchs = dao.getTagesverbrauch();
 
 		String viewname = "";
